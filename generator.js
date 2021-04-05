87392ca07863cb037af92325922f6dea409538fb
my7h1cal <72055071+my7h1cal@users.noreply.github.com>
2021-04-05 00:24:36 -0400

    Added app type and formats

--- generator.js
+++ generator.js
@@ -2,6 +2,21 @@ const type_app = require('./modules/type_app_format.js');
 const app_rules = require('./modules/rules.js');
 const app_ideas = require('./modules/ideas.js');
 
-console.log(type_app.type[0]);
 console.log(app_rules.rule.test[1]);
 console.log(app_ideas.idea[1]);
+
+function randomAppType() {
+    random_num = Math.floor(Math.random() * 3);
+    return type_app.type[random_num];
+}
+
+app_type = randomAppType();
+console.log(`Your app type is: ${app_type}`);
+
+if (app_type === "Game") {
+    console.log(type_app.format.game("Test Game Idea", "Test Game Rule"));
+} else if (app_type === "Website") {
+    console.log(type_app.format.website("Test Website Idea", "Test Website Rule"));
+} else if (app_type === "Generator") {
+    console.log(type_app.format.generator("Test Generator Idea", "Test Generator Rule"));
+}
--- modules/type_app_format.js
+++ modules/type_app_format.js
@@ -1,5 +1,15 @@
-var typesOfApp = ["Test"];
+var typesOfApp = ["Game", "Website", "Generator"];
 
-var formats = {};
+var formats = {
+    game(idea, rule) {
+        return `Make a game about ${idea}, with the rule "${rule}".`;
+    },
+    website(idea, rule) {
+        return `Build a website based on ${idea}, but your rule is "${rule}".`;
+    },
+    generator(idea, rule) {
+        return `Create a generator that outputs ${idea}, with the rule "${rule}".`;
+    }
+};
 
 module.exports = {type: typesOfApp, format: formats};
